48b80b499c1a7226ca7d4c98e5cb45dfc91719b2
Daniel Kehoe <kehoe@fortuity.com>
2011-05-08 11:51:18 -0700

    don't generate spec tests for views and helpers

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Rails3DeviseRspecCucumber
   class Application < Rails::Application
+    
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+    end
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
