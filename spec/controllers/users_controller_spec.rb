e28309277aad91b2d74b56ce1565519a76178591
Daniel Kehoe <kehoe@fortuity.com>
2011-05-08 11:44:16 -0700

    minor improvements to UsersController specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,15 +3,22 @@ require 'spec_helper'
 describe UsersController do
 
   before (:each) do
-    @user = Factory.create(:user)
+    @user = Factory(:user)
     sign_in @user
   end
 
   describe "GET 'show'" do
+    
     it "should be successful" do
-      get 'show', :id => @user.id
+      get :show, :id => @user
       response.should be_success
     end
+    
+    it "should find the right user" do
+      get :show, :id => @user
+      assigns(:user).should == @user
+    end
+    
   end
 
 end
