4214a173ececa9faca61f57272289a8244b65b09
Daniel Kehoe <daniel@danielkehoe.com>
2011-11-15 12:43:12 -0800

    eliminate use of Given/When/Then/And within a step to reference another step for Cucumber

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -10,48 +10,48 @@ Given /^I am a user named "([^"]*)" with an email "([^"]*)" and password "([^"]*
 end
 
 Then /^I should be already signed in$/ do
-  And %{I should see "Logout"}
+  step %{I should see "Logout"}
 end
 
 Given /^I am signed up as "(.*)\/(.*)"$/ do |email, password|
-  Given %{I am not logged in}
-  When %{I go to the sign up page}
-  And %{I fill in "Email" with "#{email}"}
-  And %{I fill in "Password" with "#{password}"}
-  And %{I fill in "Password confirmation" with "#{password}"}
-  And %{I press "Sign up"}
-  Then %{I should see "You have signed up successfully. If enabled, a confirmation was sent to your e-mail."}
-  And %{I am logout}
+  step %{I am not logged in}
+  step %{I go to the sign up page}
+  step %{I fill in "Email" with "#{email}"}
+  step %{I fill in "Password" with "#{password}"}
+  step %{I fill in "Password confirmation" with "#{password}"}
+  step %{I press "Sign up"}
+  step %{I should see "You have signed up successfully. If enabled, a confirmation was sent to your e-mail."}
+  step %{I am logout}
 end
 
 Given /^I am logout$/ do
-  Given %{I sign out}
+  step %{I sign out}
 end
 
 Given /^I am not logged in$/ do
-  Given %{I sign out}
+  step %{I sign out}
 end
 
 When /^I sign in as "(.*)\/(.*)"$/ do |email, password|
-  Given %{I am not logged in}
-  When %{I go to the sign in page}
-  And %{I fill in "Email" with "#{email}"}
-  And %{I fill in "Password" with "#{password}"}
-  And %{I press "Sign in"}
+  step %{I am not logged in}
+  step %{I go to the sign in page}
+  step %{I fill in "Email" with "#{email}"}
+  step %{I fill in "Password" with "#{password}"}
+  step %{I press "Sign in"}
 end
 
 Then /^I should be signed in$/ do
-  Then %{I should see "Signed in successfully."}
+  step %{I should see "Signed in successfully."}
 end
 
 When /^I return next time$/ do
-  And %{I go to the home page}
+  step %{I go to the home page}
 end
 
 Then /^I should be signed out$/ do
-  And %{I should see "Sign up"}
-  And %{I should see "Login"}
-  And %{I should not see "Logout"}
+  step %{I should see "Sign up"}
+  step %{I should see "Login"}
+  step %{I should not see "Logout"}
 end
 
 Then /^I sign out$/ do
