71cd6567a0d73d9c145073bc2ff23a8753fdbbf2
rowan <rowanu@gmail.com>
2011-11-14 21:50:18 -0500

    removed web_steps.rb. added custom steps to user_steps.rb (now uses Capybara API)

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -57,3 +57,50 @@ Then /^I should be signed out$/ do
   And %{I should see "Login"}
   And %{I should not see "Logout"}
 end
+
+When /^I go to the sign in page$/ do
+  visit '/users/sign_in'
+end
+
+Then /^I should see "([^"]*)"$/ do |text|
+  page.should have_content(text)
+end
+
+Then /^I should not see "([^"]*)"$/ do |text|
+  page.should_not have_content(text)
+end
+
+Then /^I go to the home page$/ do
+  visit '/'
+end
+
+Given /^I am on the home page$/ do
+  visit '/'
+end
+
+Given /^I go to the sign up page$/ do
+  visit '/users/sign_up'
+end
+
+Given /^I fill in the following:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  table.rows_hash.each do |key, value|
+    fill_in(key, :with => value)
+  end
+end
+
+When /^I press "([^"]*)"$/ do |label|
+  click_button label
+end
+
+When /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in(field, :with => value)
+end
+
+When /^I go to the homepage$/ do
+  visit '/'
+end
+
+When /^I follow "([^"]*)"$/ do |text|
+  click_link text
+end
