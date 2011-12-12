bb800b88b59f352f8b4a919b3b15a146131f2bbd
Cole <thecatwasnot@gmail.com>
2011-12-12 12:47:17 -0600

    Move all step defs to capybara and remove unused

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -10,48 +10,33 @@ Given /^I am a user named "([^"]*)" with an email "([^"]*)" and password "([^"]*
 end
 
 Then /^I should be already signed in$/ do
-  step %{I should see "Logout"}
-end
-
-Given /^I am signed up as "(.*)\/(.*)"$/ do |email, password|
-  step %{I am not logged in}
-  step %{I go to the sign up page}
-  step %{I fill in "Email" with "#{email}"}
-  step %{I fill in "Password" with "#{password}"}
-  step %{I fill in "Password confirmation" with "#{password}"}
-  step %{I press "Sign up"}
-  step %{I should see "You have signed up successfully. If enabled, a confirmation was sent to your e-mail."}
-  step %{I am logout}
-end
-
-Given /^I am logout$/ do
-  step %{I sign out}
+  page.should have_content "Logout"
 end
 
 Given /^I am not logged in$/ do
-  step %{I sign out}
+  visit '/users/sign_out'
 end
 
 When /^I sign in as "(.*)\/(.*)"$/ do |email, password|
-  step %{I am not logged in}
-  step %{I go to the sign in page}
-  step %{I fill in "Email" with "#{email}"}
-  step %{I fill in "Password" with "#{password}"}
-  step %{I press "Sign in"}
+  visit '/users/sign_out'
+  visit '/users/sign_in'
+  fill_in "Email", :with => email
+  fill_in "Password", :with => password
+  click_button "Sign in"
 end
 
 Then /^I should be signed in$/ do
-  step %{I should see "Signed in successfully."}
+  page.should have_content "Signed in successfully."
 end
 
 When /^I return next time$/ do
-  step %{I go to the home page}
+  visit '/'
 end
 
 Then /^I should be signed out$/ do
-  step %{I should see "Sign up"}
-  step %{I should see "Login"}
-  step %{I should not see "Logout"}
+  page.should have_content "Sign up"
+  page.should have_content "Login"
+  page.should_not have_content "Logout"
 end
 
 Then /^I sign out$/ do
@@ -64,10 +49,6 @@ end
 
 Then /^I should see "([^"]*)"$/ do |text|
   page.should have_content(text)
-end
-
-Then /^I should not see "([^"]*)"$/ do |text|
-  page.should_not have_content(text)
 end
 
 Then /^I go to the home page$/ do
