bcaee2d9729cb9b5a8324e9e58c93e2d019cc756
Daniel Kehoe <daniel@danielkehoe.com>
2012-02-10 16:47:35 -0800

    spelling errors not allowed in this repo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110423013550) do
+ActiveRecord::Schema.define(:version => 20120129231333) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
