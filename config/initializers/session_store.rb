5cc9057105fe8f59e97d8df773f6581502105bc4
Daniel Kehoe <daniel@danielkehoe.com>
2012-07-12 18:59:32 -0700

    conform to app generated by version 1.5.5 of rails_apps_composer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Rails3DeviseRspecCucumber::Application.config.session_store :cookie_store, :key => '_rails3-devise-rspec-cucumber_session'
+Rails3DeviseRspecCucumber::Application.config.session_store :cookie_store, key: '_rails3-devise-rspec-cucumber_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
