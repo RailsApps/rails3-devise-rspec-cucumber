11b984b02ef74332c0e15cfb732ddac8a4d89f16
Daniel Kehoe <daniel@danielkehoe.com>
2013-02-20 16:42:28 -0800

    build using rails_apps_composer 2.2.34

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,7 +34,7 @@ Rails3DeviseRspecCucumber::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-  
+
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => 'example.com' }
 
